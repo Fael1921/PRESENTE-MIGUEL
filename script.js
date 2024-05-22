e77fcbbcbde63043ebcff746fc4434a7a2490fa2
Fael1921 <rd083765@gmail.com>
2024-05-22 10:30:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('gift').addEventListener('click', function() {
     document.getElementById('message').style.display = 'block';
     // Substitua o URL abaixo pelo URL da imagem de fundo que você deseja usar
-    document.body.style.backgroundImage = 'url("https://www.mundoconectado.com.br/wp-content/uploads/2023/10/Cruzeiro-x-Flamengo-pelo-Brasileirao-2023-onde-assistir-ao-vivo.jpg")';
+    document.body.style.backgroundImage = 'url("https://c4.wallpaperflare.com/wallpaper/83/876/997/cruzeiro-esporte-clube-brazil-soccer-soccer-clubs-wallpaper-preview.jpg")';
     document.body.style.backgroundSize = 'cover'; // Ajusta a imagem para cobrir todo o fundo
     document.body.style.backgroundPosition = 'center'; // Centraliza a imagem
     document.getElementById('gift').style.display = 'none'; // Oculta a imagem do presente
@@ -10,4 +10,3 @@ document.getElementById('gift').addEventListener('click', function() {
     particlesJS.load('particles-js', 'particles-config.json', function() {
         console.log('particles.js loaded - callback');
     });
-});
